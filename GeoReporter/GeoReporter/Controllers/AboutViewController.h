a8dc76800b638b8a00b0419baeb3765e0438cdc9
Cliff Ingham <inghamn@bloomington.in.gov>
2013-01-23 16:42:03 -0500

    Added the AboutView

---
+++ GeoReporter/GeoReporter/Controllers/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  GeoReporter
+//
+//  Created by Cliff Ingham on 1/23/13.
+//  Copyright (c) 2013 City of Bloomington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
