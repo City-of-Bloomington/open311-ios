2b3e0059663ab23ba1745dfafa73cc93de158721
Cliff Ingham <inghamn@bloomington.in.gov>
2013-02-20 10:38:17 -0500

    Updated copyright and license statement in all source code files.

--- GeoReporter/GeoReporter/Controllers/AboutController.h
+++ GeoReporter/GeoReporter/Controllers/AboutController.h
@@ -1,10 +1,13 @@
-//
-//  AboutController.h
-//  GeoReporter
-//
-//  Created by Cliff Ingham on 1/25/13.
-//  Copyright (c) 2013 City of Bloomington. All rights reserved.
-//
+/**
+ * @copyright 2013 City of Bloomington, Indiana. All Rights Reserved
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @license http://www.gnu.org/licenses/gpl.txt GNU/GPLv3, see LICENSE.txt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 #import <UIKit/UIKit.h>
 
--- GeoReporter/GeoReporter/Controllers/ReportFields/TextEntryDelegate.h
+++ GeoReporter/GeoReporter/Controllers/ReportFields/TextEntryDelegate.h
@@ -1,13 +1,19 @@
-//
-//  TextEntryDelegate.h
-//  GeoReporter
-//
-//  Created by Cliff Ingham on 2/6/13.
-//  Copyright (c) 2013 City of Bloomington. All rights reserved.
-//
-// The bulk of our report views just ask the user for one
-// value at a time.  No matter what the Open311 type, the
-// user is really only providing a single string per field.
+/**
+ * Protocol used to send user input back to ReportController
+ *
+ * The bulk of our report views just ask the user for one
+ * value at a time.  No matter what the Open311 type, the
+ * user is really only providing a single string per field.
+ *
+ * @copyright 2013 City of Bloomington, Indiana. All Rights Reserved
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @license http://www.gnu.org/licenses/gpl.txt GNU/GPLv3, see LICENSE.txt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 #import <Foundation/Foundation.h>
 
