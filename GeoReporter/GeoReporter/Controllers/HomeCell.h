06b6ebd0670ce9c5491e8f2a0d0b297cb6fb4c1e
Marius Constantinescu <constantinescu.marius@gmail.com>
2013-09-08 11:48:47 +0200

    Customized HomeController for iPad

---
+++ GeoReporter/GeoReporter/Controllers/HomeCell.h
@@ -0,0 +1,16 @@
+//
+//  HomeCell.h
+//  GeoReporter
+//
+//  Created by Marius Constantinescu on 9/7/13.
+//  Copyright (c) 2013 City of Bloomington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+#define HOME_CELL_IPAD_OFFSET 100
+
+@interface HomeCell : UITableViewCell
+
+@end
